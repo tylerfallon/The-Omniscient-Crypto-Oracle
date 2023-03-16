6367a0b6725feb286562633e5beaf82e363eced2
tylerfallon <tylermfallon@gmail.com>
2023-03-16 04:30:08 -0400

    updated color schemes and the market type logic

--- templates/assets/js/cryptodata.js
+++ templates/assets/js/cryptodata.js
@@ -1,1 +1,41 @@
-console.log('hello')
+async function initcryptodata() {
+    console.log('test1')
+    let response = await fetch('/getcryptodata')
+    const cryptodata = await response.json();
+    return cryptodata;
+  }
+  
+  async function updateCryptoPrice() {
+    let cryptoDataUpdated = await initcryptodata();
+    document.querySelector('#cryptoprice').innerHTML = '$' + cryptoDataUpdated.price;
+  }
+  
+  
+  async function initcryptopercentchange() {
+    console.log('test2')
+    let response = await fetch('/getpercentchange')
+    const cryptodata = await response.json();
+    return cryptodata;
+  }
+  
+  async function updatePercentChange() {
+    let percentChangeUpdated = await initcryptopercentchange();
+    document.querySelector('#btcpercentchange').innerHTML = percentChangeUpdated.price + '%';
+  }
+  
+  
+  async function initMarketType() {
+    console.log('test3')
+    let response = await fetch('/getmarkettype')
+    const cryptodata = await response.json();
+    return cryptodata;
+  }
+  
+  async function updateMarketType() {
+    let marketType = await initMarketType();
+    document.querySelector('#bullvsbear').innerHTML = marketType;
+  }
+  
+  updateCryptoPrice();
+  updatePercentChange();
+  updateMarketType();
